67a270e8dd2cb155cf6c8f421cd7a9cd2e0ff9df
Alex_K <alexkushndv@gmail.com>
2021-09-04 21:06:55 +0300

    remove default root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::API
   include Authenticable
+
+  def not_found
+    render plain: 'Not found.', status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'application#not_found'
   # Api definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
