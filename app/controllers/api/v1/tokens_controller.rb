8f3ce97dac608b42c78cff681eefd1b3e0c3f853
Alex_K <alexkushndv@gmail.com>
2021-09-04 19:06:33 +0300

    add feature jwt token for user

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TokensController < ApplicationController
+      def create
+        @user = User.find_by(email: user_params[:email])
+        if @user&.authenticate(user_params[:password])
+          render json: {
+            token: JsonWebToken.encode(user_id: @user.id),
+            email: @user.email
+          }
+        else
+          head :unauthorized
+        end
+      end
+
+      private
+
+      # Only allow a trusted parameter "allow list" through.
+      def user_params
+        params.permit(:email, :password)
+      end
+    end
+  end
+end
