f11a6e9981499158cd8f3d5668c4c66d4a481480
Alex_K <alexkushndv@gmail.com>
2021-09-05 14:41:35 +0300

    implemented seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Article.delete_all
+User.delete_all
+
+def category_name(user)
+  user.id < 2 ? 'Mystery' : 'Western'
+end
+
+3.times do
+  user = User.create! email: Faker::Internet.email, password: '123456'
+  puts "Created a new user: #{user.email}"
+  2.times do
+    article = Article.create!(
+      title: Faker::Book.title,
+      content: Faker::Lorem.paragraph_by_chars(number: 656, supplemental: false),
+      category: category_name(user),
+      date: Faker::Date.between(from: '2021-09-01', to: '2021-09-05'),
+      user_id: user.id
+    )
+    puts "Created an article: #{article.title}"
+  end
+end
